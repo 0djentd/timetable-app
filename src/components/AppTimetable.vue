6c366ffecaad64f4553ac292b174667458a46e07
0djentd <58089540+0djentd@users.noreply.github.com>
2022-08-14 10:08:44 +0300

    Initial commit

---
+++ src/components/AppTimetable.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <v-data-table :headers="headers" :items="tasks" :items-per-page="50">
+    </v-data-table>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      headers: [
+        {
+          text: "Task title",
+          align: "start",
+          value: "title",
+        },
+        { text: "Description", value: "description" },
+        { text: "Start", value: "start" },
+        { text: "End", value: "end" },
+        { text: "Priority", value: "priority" },
+      ],
+      tasks: [],
+    };
+  },
+};
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <AppTimetable />
+  </div>
+</template>
+
+<script>
+import AppTimetable from "@/components/AppTimetable.vue";
+
+export default {
+  components: {
+    AppTimetable,
+  },
+};
+</script>
