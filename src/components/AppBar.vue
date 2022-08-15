974e99a39dc1b8d2d886da506246f3ec32089659
0djentd <58089540+0djentd@users.noreply.github.com>
2022-08-15 10:14:38 +0300

    new file:   src/components/AppBar.vue

---
+++ src/components/AppBar.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-app-bar app color="white" flat>
+    <v-tabs centered class="ml-n9" color="grey darken-1">
+      <v-tab to="/"> Timetable </v-tab>
+      <v-tab href="https://github.com/0djentd/timetable-app">
+        Repository
+      </v-tab>
+      <v-tab to="/about/"> About </v-tab>
+    </v-tabs>
+  </v-app-bar>
+</template>
+
+<script>
+export default {};
+</script>
