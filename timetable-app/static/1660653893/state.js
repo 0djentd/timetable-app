f1fcc9c1cedaad9ec707bc0439481d447b363df3
0djentd <58089540+0djentd@users.noreply.github.com>
2022-08-16 15:45:22 +0300

    new file:   200.html new file:   index.html new file:   timetable-app/022fbb4.js new file:   timetable-app/04e83f3.js new file:   timetable-app/5990034.js new file:   timetable-app/608e5cb.js new file:   timetable-app/8f53329.js new file:   timetable-app/LICENSES new file:   timetable-app/c2772c6.js new file:   timetable-app/e29f5d9.js new file:   timetable-app/e611a90.js new file:   timetable-app/manifest.3bcf6480.json new file:   timetable-app/static/1660653893/manifest.js new file:   timetable-app/static/1660653893/payload.js new file:   timetable-app/static/1660653893/state.js

---
+++ timetable-app/static/1660653893/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Ftimetable-app\u002Ftimetable-app\u002Fstatic\u002F1660653893",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Ftimetable-app\u002F",assetsPath:"\u002Ftimetable-app\u002Ftimetable-app\u002F",cdnURL:a}}}}(null));
